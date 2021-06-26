ff474c01eb98f77ea09946b68a024a2bf7f4bdb5
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-26 03:38:37 -0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,8 @@
-
+#!/usr/bin/python3
+"""
+    __init__ modules
+"""
+from models.engine.file_storage import FileStorage
+from .base_model import BaseModel
+storage = FileStorage()
+storage.reload()
