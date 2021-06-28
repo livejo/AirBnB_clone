6c5b646394f167acc56d53c0caca07b91c7d852b
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-28 16:14:27 +0300

    Update console.py

--- console.py
+++ console.py
@@ -7,6 +7,8 @@ import os
 import models
 from models.base_model import BaseModel
 
+storage = models.storage
+
 
 class HBNBCommand(cmd.Cmd):
     """ a command line interpreter """
@@ -84,6 +86,105 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
             else:
                 print('** no instance found **')
+
+    def do_all(self, s):
+        """ Prints all string representation of all instances """
+        storage.reload()
+        arg = s.split()
+        lists = []
+        cont = storage.all()
+        if len(arg) == 0:
+            for arg_id in cont.keys():
+                obj = cont[arg_id]
+                lists.append(str(obj))
+            print(lists)
+            return
+        if len(arg) == 1:
+             try:
+                 eval(arg[0])
+                 for arg_id in cont.keys():
+                     if type(cont[arg_id] is eval(arg[0])):
+                         obj = cont[arg_id]
+                         lists.append(str(obj))
+                 print(lists)
+             except Exception:
+                 print("** class doesn't exist **")
+                 return
+    def do_update(self, s):
+        """
+        Update command Updates an instance based on the class name and id
+        by adding or updating attribute
+        (save the change into the JSON file). 
+        """
+        storage.reload()
+        args = s.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if len(args) >= 1:
+            try:
+                eval(args[0])
+                if len(args) == 1:
+                    print("** instance id missing **")
+                    return
+                try:
+                    key_id = args[0] + "." + args[1]
+                    container_obj = storage.all()
+                    container_obj[key_id]
+                except Exception:
+                    print("** no instance found **")
+                    return
+            except Exception:
+                print("** class doesn't exist **")
+                return
+        if len(args) >= 2:
+            try:
+                eval(args[0])
+                if len(args) == 2:
+                    print("** attribute name missing **")
+                    return
+            except Exception:
+                print("** class doesn't exist **")
+                return
+
+        if len(args) >= 3:
+            try:
+                eval(args[0])
+                if len(args) == 3:
+                    print("** value missing **")
+                    return
+            except Exception:
+                print("** class doesn't exist **")
+                return
+        if len(args) >= 4:
+            args = " ".join(str(arg).split(maxsplit=3)).split(maxsplit=3)
+
+            class_name = args[0]
+            obj_id = args[1]
+            obj_attr = args[2].strip("\"\'")
+            obj_new_val = args[3]
+            try:
+                eval(class_name)
+                key_id = class_name + "." + obj_id
+                container_obj = storage.all()
+                if key_id in container_obj:
+                    obj = container_obj[key_id]
+                    try:
+                        type_attr = type(getattr(obj, obj_attr))
+                        obj_new_val = self.same_type_as_attr(obj_new_val,
+                                                             type_attr)
+                        if obj_new_val is False:
+                            return
+                    except Exception:
+                        obj_new_val = self.convert_new_val(obj_new_val)
+                    setattr(obj, obj_attr, obj_new_val)
+                    storage.save()
+                else:
+                    print("** no instance found **")
+                    return
+            except Exception as ex:
+                print("** class doesn't exist **")
+
             
             
 
