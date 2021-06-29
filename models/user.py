a1b547b56003d1030c3fe75bb36587d5d24540e1
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-29 13:34:04 +0300

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ class User that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+
+class User(BaseModel):
+    """ defining User class """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+            Init
+        """
+        super().__init__(*args, **kwargs)
