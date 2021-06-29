9b1bf9fad4175f368e1f786bd3e641991aa988f3
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-29 13:57:37 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -9,6 +9,7 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """ defining User class """
     name = ""
+    state_id = ""
 
     def __init__(self, *args, **kwargs):
         """
