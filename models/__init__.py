f220356b5d3cb0b9a9c7387f322bf91e387e1e9f
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-29 14:13:03 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,12 @@
 """
 from models.engine.file_storage import FileStorage
 from .base_model import BaseModel
+from .user import User
+from .review import Review
+from .city import City
+from .amenity import Amenity
+from .place import Place
+from .state import State
+
 storage = FileStorage()
 storage.reload()
