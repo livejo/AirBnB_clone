9a76cfd570dec734f05e4c49b6085d862c18f24d
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-29 13:56:40 +0300

    Create city.py

---
+++ models/city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ class User that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+
+class City(BaseModel):
+    """ defining User class """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+            Init
+        """
+        super().__init__(*args, **kwargs)
