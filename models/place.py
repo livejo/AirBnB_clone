6f1aeb2cdcd944dde3b19c7d2cec5f23f4aa0b03
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-30 10:15:02 -0700

    Update place.py

--- models/place.py
+++ models/place.py
@@ -20,9 +20,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
-    def __init__(self, *args, **kwargs):
-        """
-            init
-        """
-        super().__init__(*args, **kwargs)
