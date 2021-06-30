ed0ab7dd5233b959108dbe38844bc4e9260706b5
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-30 23:40:21 +0300

    Create console.py

--- console.py
+++ console.py
@@ -156,6 +156,24 @@ class HBNBCommand(cmd.Cmd):
 
         models.storage.all()[query_key].save()
 
+    def default(self, inp):
+        """Retrieve all instances class using: <class name>.all()"""
+        count = 0
+        words = inp.split(".")
+
+        if words[0] in classes_dict and words[1] == "all()":
+            self.do_all(words[0])
+        elif words[0] in classes_dict and words[1] == "count()":
+            if (words[0] not in classes_dict):
+                print("** class doesn't exist **")
+                return (False)
+            else:
+                for key in models.storage.all():
+                    if key.startswith(words[0]):
+                        count += 1
+                print(count)
+        else:
+            print("*** Unknown syntax: {}".format(inp))
 
 
 if __name__ == '__main__':
