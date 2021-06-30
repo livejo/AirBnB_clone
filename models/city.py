c47e08e6a97d206f3cf32b30b073be6fd2364d5c
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-30 10:14:35 -0700

    Update city.py

--- models/city.py
+++ models/city.py
@@ -9,9 +9,3 @@ class City(BaseModel):
     """ defining City class """
     name = ""
     state_id = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-            Init
-        """
-        super().__init__(*args, **kwargs)
