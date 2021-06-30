b7fa18a18c79afefa4ef3b6cf2648680d5b52885
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-30 10:15:25 -0700

    Update review.py

--- models/review.py
+++ models/review.py
@@ -10,9 +10,3 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-            Init
-        """
-        super().__init__(*args, **kwargs)
