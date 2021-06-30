227aaed2c5b99124b6c311cec5b0c93f02812069
livejo <yoseph.kefale@gmail.com>
2021-06-30 22:33:30 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,3 +5,6 @@ from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
+from models.state import State
+from models.review import Review
+from models.user import User
