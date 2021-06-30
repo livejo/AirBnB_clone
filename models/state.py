cc3735060000e65394cf8b5f142c849102d51384
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-30 10:15:44 -0700

    Update state.py

--- models/state.py
+++ models/state.py
@@ -8,9 +8,3 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ defining State class """
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-            Init
-        """
-        super().__init__(*args, **kwargs)
