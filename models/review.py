d9837afa92793d19379c18884ac4f97155463385
livejo <yoseph.kefale@gmail.com>
2021-06-30 21:26:58 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,1 @@
 #!/usr/bin/python3
-"""
- class Review that inherits from BaseModel
-"""
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    """ defining User class """
-    place_id = ""
-    user_id = ""
-    text = ""
