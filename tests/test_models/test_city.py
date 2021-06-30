4bfababbf46d41175ad0fad0af00ca416f3aaabc
livejo <yoseph.kefale@gmail.com>
2021-06-30 22:33:20 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 """Test City"""
 import unittest
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
